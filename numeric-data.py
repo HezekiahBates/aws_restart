98581408b9140599a36ce618892503676b204626
HezekiahBates <63075581+HezekiahBates@users.noreply.github.com>
2022-06-17 14:27:56 -0700

    Add files via upload

---
+++ numeric-data.py
@@ -0,0 +1,27 @@
+print("Python has three bumeric types: int, float, and complex");
+myValue=1;
+print(myValue);
+print(type(myValue));
+print(str(myValue) + " is of the data type " + str(type(myValue)));
+
+myFloat=3.14;
+print(myFloat);
+print(type(myFloat));
+print(str(myFloat) + " is of the data type " + str(type(myFloat)));
+
+myComplex=5j;
+print(myComplex);
+print(type(myComplex));
+print(str(myComplex) + " is of the data type " + str(type(myComplex)));
+
+myTrue= True;
+print(myTrue);
+print(type(myTrue));
+print(str(myTrue) + " is of the data type " + str(type(myTrue)));
+
+myFalse= False;
+print(myFalse);
+print(type(myFalse));
+print(str(myFalse) + " is of the data type " + str(type(myFalse)));
+
+
---
+++ string-data-type.py
@@ -0,0 +1,19 @@
+# exrecise 1
+myString = "This is a string.";
+print(myString);
+print(type(myString));
+print(myString + " is of the data type " + str(type(myString)));
+#exercise 2
+firstString = "water";
+secondString = "fall";
+thirdString = firstString + secondString;
+print(thirdString);
+#exercise 3
+name = input("What is your name? ");
+print(name);
+
+# Exercise 4
+color = input("What is your favorite color? ");
+animal = input("what is your favorite animal? ");
+print("{}, you like a {} {}!".format(name,color,animal));
+
